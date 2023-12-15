6b205641bdf803d6e0a3a9d87da0e7612e85f6e9
Freddy0406 <creeperqaqoao@gmail.com>
2023-12-15 13:51:29 +0800

    Path loss fix (-30dB into linear)

--- main.m
+++ main.m
@@ -3,7 +3,7 @@ clear;
 close all;
 
 avg_N = 1000;                         % Average times
-variance_arr = [0.05 0.02 0.01];    % Figure 1 with different variance
+variance_arr = [0.05 0.01 0.002];    % Figure 1 with different variance
 tx_power_arr = [round(power(10,0),5) round(power(10,0.5),5) round(power(10,1),5) round(power(10,1.5),5) round(power(10,2),5)];
 % Figure 1 with different transmit power
 
@@ -12,7 +12,7 @@ avg_mse_nonrobust = zeros(3,5);
 iteration = avg_N;                  %parfor iteration
 
 
-%% To make figure 1
+%% To make figure 2
 
 ppm = ParforProgressbar(iteration);
 ppm = ParforProgressbar(iteration, 'showWorkerProgress', true);
@@ -36,4 +36,16 @@ delete(ppm);
 
 avg_mse_robust = avg_mse_robust./avg_N;
 avg_mse_nonrobust = avg_mse_nonrobust./avg_N;
+
+tx_power_arr = 10*log10(tx_power_arr);
+
+
+
+figure(1)
+semilogy(tx_power_arr,avg_mse_robust(1,:),'r-o',tx_power_arr,avg_mse_robust(2,:),'b-square',tx_power_arr,avg_mse_robust(3,:),'k-diamond',tx_power_arr,avg_mse_nonrobust(1,:),'r--x',tx_power_arr,avg_mse_nonrobust(2,:),'g--^',tx_power_arr,avg_mse_nonrobust(3,:),'m--+');
+xlabel("Transmit Power (dBm)")
+legend('Robust, {\sigma^2}=0.05','Robust, {\sigma^2}=0.01','Robust, {\sigma^2}=0.002','Non-Robust, {\sigma^2}=0.05','Non-Robust, {\sigma^2}=0.01','Non-Robust, {\sigma^2}=0.002')
+
+
+%% To make figure 3
 
--- mmse.m
+++ mmse.m
@@ -5,7 +5,7 @@ function [mse,t]=mmse(N,variance,P0,mode,bit_of_phase)
     %variance var_g=var_r=var_d=var
     M = 4;                          %BS antenna amount
     s = randn(1,1,"like",1i);       %Symbol for user(zero-meam, unit power=>variance=1)
-    L0 = -30;                       %Path loss dB           
+    L0 = 10^(-30/10);               %Path loss dB -30dB => linear           
     AP_loc = [0 0];                 %AP location
     IRS_loc = [100,0];              %IRS location
     UE_loc = [100 20];              %User location
@@ -33,7 +33,7 @@ function [mse,t]=mmse(N,variance,P0,mode,bit_of_phase)
     noise_var = power(10,-11);                      %var_n = 110dBm = 10^-11 mW
     noise = sqrt(noise_var)*randn(1,1,"like",1i);   %Generate random 1X1 noise
 
-        % Initialize w 
+    % Initialize w 
     w = zeros(M,1);
     for i = 1:M
         w(i) = sqrt(P0)/sqrt(M);
